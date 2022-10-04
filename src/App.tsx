9cc2b6803f80cb3284de6837504597965f61b441
leida <leida@bytedance.com>
2022-10-04 23:07:37 +0800

    feat: create request

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.less';
 
 function App() {
+  useEffect(() => {
+    const code = 'public class HelloWorld { public static void main(String []args) { System.out.println("Hello Wo333rld!"); } }'
+
+    const body = new URLSearchParams();
+    body.set('code', code);
+    body.set('fileext', 'java');
+    body.set('language', '8');
+    body.set('stdin', '');
+    body.set('token', '4381fe197827ec87cbac9552f14ec62a');
+    const response = fetch('/compile2.php', {
+      method: 'POST',
+      body: body.toString(),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      mode: 'cors',
+    }).then(res => res.json());
+    console.log('dada: ', response);
+  });
+
+  const renderCompiler = () => {
+    return (
+      <iframe
+        className='iframe'
+        src='https://tool.lu/coderunner/'
+      />
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {renderCompiler()} */}
     </div>
   );
 }
